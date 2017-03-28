b17d059d84d7786ab7da5e55fde5ce592923102b
kvyatkovskymike <kvyatkovskymike@gmail.com>
2017-03-28 23:23:17 +0300

    NightCommit
    23:23

--- EnemyEngineer.cs
+++ EnemyEngineer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyEngineer : MonoBehaviour {
 	[SerializeField]
-	private float speed = 0.5f;
+	public float speed = 0.5f;
 	public bool CheckEnemy = false;
 	private SpriteRenderer sprite;
 	// Use this for initialization
