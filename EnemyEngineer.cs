d126f24122a7e75f55d3d33fffd5c534ff2c3095
kvyatkovskymike <kvyatkovskymike@gmail.com>
2017-03-28 21:15:25 +0300

    s0_0_1

--- EnemyEngineer.cs
+++ EnemyEngineer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dsghedshsd : MonoBehaviour {
+public class EnemyEngineer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,8 +11,6 @@ public class dsghedshsd : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		gdsfhserfhedrheda
 		
 	}
 }
