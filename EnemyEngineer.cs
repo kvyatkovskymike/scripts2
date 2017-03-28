be558508cc8a7ef91fba6bdb77c0dd0918c122e4
kvyatkovskymike <kvyatkovskymike@gmail.com>
2017-03-28 23:09:08 +0300

    EnemyCanMove
    He can move)

--- EnemyEngineer.cs
+++ EnemyEngineer.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyEngineer : MonoBehaviour {
-
+	[SerializeField]
+	private float speed = 0.5f;
+	public bool CheckEnemy = false;
+	private SpriteRenderer sprite;
 	// Use this for initialization
-	void Start () {
-		
+
+	public void Awake() 
+	{
+		sprite = GetComponentInChildren<SpriteRenderer> ();
 	}
-	
+
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.CompareTag ("Player")) 
+		{
+			CheckEnemy = true;
+		}
+	}
+
+	public void Move()
+	{
+	Vector3 direction = transform.right;
+
+	transform.position = Vector3.MoveTowards (transform.position, transform.position + direction, speed * Time.deltaTime);
+
+	sprite.flipX = direction.x < 0.0f;
+	}
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		Move ();
+		if (CheckEnemy = true) 
+		{
+			
+		}
 	}
 }
